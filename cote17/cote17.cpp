6bfc1e89bddbfc074c97cb388be84054a1718d28
ohne <ghdlr785@gmail.com>
2022-06-25 18:23:26 +0900

    초기 커밋.

---
+++ cote17/cote17.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// stoi 함수 사용법
+
+int solution(string s) {
+    int answer = 0;
+    
+    answer = stoi(s);
+    std::cout << answer << endl;
+    return answer;
+}
+
+int main()
+{
+}
